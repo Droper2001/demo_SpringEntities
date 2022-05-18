d2a488ceb05d36fb17abc2645b62ca9a73eb0b9a
Droper2001 <nouhailahanbali@gmail.com>
2022-05-18 19:48:44 +0200

    Initial commit

---
+++ src/test/java/com/example/demo_springentities/DemoSpringEntitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_springentities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringEntitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
